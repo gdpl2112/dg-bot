3b69117cd8803553c25977e68ad0e23844f62c85
HRS-Computer <3474006766@qq.com>
2023-07-18 21:36:44 +0800

    add part 1 cron manager

--- src/main/java/io/github/gdpl2112/dg_bot/controllers/UserController.java
+++ src/main/java/io/github/gdpl2112/dg_bot/controllers/UserController.java
@@ -63,7 +63,6 @@ public class UserController {
         return null;
     }
 
-
     //==============================================================
     @Autowired
     AdministratorMapper administratorMapper;
