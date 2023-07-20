b8cd3a4128b19d6e8331ed88d02d002a31575c2c
HRS-Computer <3474006766@qq.com>
2023-07-20 21:00:17 +0800

    fix auth

--- src/main/java/io/github/gdpl2112/dg_bot/BaseComponent.java
+++ src/main/java/io/github/gdpl2112/dg_bot/BaseComponent.java
@@ -1,9 +1,14 @@
 package io.github.gdpl2112.dg_bot;
 
+import io.github.gdpl2112.dg_bot.dao.AuthM;
+import io.github.gdpl2112.dg_bot.mapper.AuthMapper;
 import io.github.kloping.MySpringTool.h1.impl.LoggerImpl;
 import io.github.kloping.MySpringTool.interfaces.Logger;
+import net.mamoe.mirai.Bot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,4 +44,17 @@ public class BaseComponent {
         return new SessionRegistryImpl();
     }
 
+    @Autowired
+    AuthMapper authMapper;
+
+    @Scheduled(cron = "0 1 1 * * ? ")
+    public void auth() {
+        for (AuthM authM : authMapper.selectList(null)) {
+            if (authM.getExp() < System.currentTimeMillis()) {
+                Long qid = Long.valueOf(authM.getQid());
+                Bot bot = Bot.getInstanceOrNull(qid);
+                if (bot != null && bot.isOnline()) bot.close();
+            }
+        }
+    }
 }
