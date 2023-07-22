5742849e1636e544921d47908445972a189f06a0
HRS-Computer <3474006766@qq.com>
2023-07-22 20:07:57 +0800

    feat flashImage send

--- src/main/java/io/github/gdpl2112/dg_bot/service/SaveService.java
+++ src/main/java/io/github/gdpl2112/dg_bot/service/SaveService.java
@@ -6,6 +6,7 @@ import io.github.gdpl2112.dg_bot.dao.GroupConf;
 import io.github.gdpl2112.dg_bot.mapper.GroupConfMapper;
 import io.github.gdpl2112.dg_bot.mapper.SaveMapper;
 import kotlin.coroutines.CoroutineContext;
+import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.contact.Friend;
 import net.mamoe.mirai.contact.Group;
@@ -14,8 +15,10 @@ import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.*;
 import net.mamoe.mirai.message.code.MiraiCode;
+import net.mamoe.mirai.message.data.FlashImage;
 import net.mamoe.mirai.message.data.Message;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
+import net.mamoe.mirai.message.data.SingleMessage;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -66,6 +69,28 @@ public class SaveService extends SimpleListenerHost {
     @EventHandler
     public void onMessage(@NotNull FriendMessageEvent event) throws Exception {
         save(AllMessage.factory(event));
+        for (SingleMessage singleMessage : event.getMessage()) {
+            if (singleMessage instanceof FlashImage) {
+                FlashImage image = (FlashImage) singleMessage;
+                Friend friend = event.getFriend();
+                if (isListening(event.getBot(), "f", friend.getId())) return;
+                MessageChainBuilder builder = new MessageChainBuilder();
+                builder.append("'").append(friend.getNick()).append("(" + friend.getId() + ")'在私聊").append("发送了[闪照]:")
+                        .append(image.getImage());
+                event.getBot().getAsFriend().sendMessage(builder.build());
+            }
+        }
+    }
+
+    private boolean isListening(Bot event, String t, long friend) {
+        QueryWrapper<GroupConf> qw = new QueryWrapper<>();
+        qw.eq("qid", event.getId());
+        qw.eq("tid", t + friend);
+        GroupConf groupConf = groupConfMapper.selectOne(qw);
+        if (groupConf != null) {
+            if (!groupConf.getK1()) return true;
+        }
+        return false;
     }
 
     @EventHandler
@@ -95,19 +120,13 @@ public class SaveService extends SimpleListenerHost {
     public void onMessage(MessageRecallEvent.GroupRecall event) {
         Group group = event.getGroup();
         Member member = event.getOperator();
-        QueryWrapper<GroupConf> qw = new QueryWrapper<>();
-        qw.eq("qid", event.getBot().getId());
-        qw.eq("tid", "g" + group.getId());
-        GroupConf groupConf = groupConfMapper.selectOne(qw);
-        if (groupConf != null) {
-            if (!groupConf.getK1()) return;
-        }
+        if (isListening(event.getBot(), "g", group.getId())) return;
         AllMessage all = getMessage(event);
         if (all != null) {
             Message m0 = MiraiCode.deserializeMiraiCode(all.getContent());
             MessageChainBuilder builder = new MessageChainBuilder();
-            builder.append("'").append(member.getNameCard()).append("(" + member.getId() + ")").append("'在群聊'").append(event.getGroup().getName())
-                    .append("(" + group.getId() + ")'撤回消息:").append(m0);
+            builder.append("'").append(member.getNameCard()).append("(" + member.getId() + ")").append("'在群聊'")
+                    .append(event.getGroup().getName()).append("(" + group.getId() + ")'撤回消息:").append(m0);
             Message message = builder.build();
             for (Contact contact : all(event)) {
                 contact.sendMessage(message);
@@ -118,13 +137,7 @@ public class SaveService extends SimpleListenerHost {
     @EventHandler
     public void onMessage(MessageRecallEvent.FriendRecall event) {
         Friend friend = event.getAuthor();
-        QueryWrapper<GroupConf> qw = new QueryWrapper<>();
-        qw.eq("qid", event.getBot().getId());
-        qw.eq("tid", "f" + friend.getId());
-        GroupConf groupConf = groupConfMapper.selectOne(qw);
-        if (groupConf != null) {
-            if (!groupConf.getK1()) return;
-        }
+        if (isListening(event.getBot(), "f", friend.getId())) return;
         AllMessage all = getMessage(event);
         if (all != null) {
             Message m0 = MiraiCode.deserializeMiraiCode(all.getContent());
