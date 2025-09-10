17270f11d3c0d5aad5c7a39f032d895d9c7275b8
Kloping <3474006766@qq.com>
2025-09-10 10:32:33 +0800

    feat: 新增 点赞黑白名单设置

--- src/main/java/io/github/gdpl2112/dg_bot/compile/CompileRes.java
+++ src/main/java/io/github/gdpl2112/dg_bot/compile/CompileRes.java
@@ -14,7 +14,7 @@ import java.util.Properties;
  */
 public class CompileRes {
 
-    public static final String VERSION_DATE = "2025/0909";
+    public static final String VERSION_DATE = "2025/0910";
 
     public static boolean isLinux() {
         String osName = System.getProperty("os.name").toLowerCase();
