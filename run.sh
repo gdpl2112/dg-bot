df389b0461349711d46e5f53ef20886535686c91
Kloping <3474006766@qq.com>
2026-01-27 16:54:06 +0800

    fix(runtime): 增加 JVM G1 垃圾回收器参数
    - 在启动命令中添加 -XX:+UseG1GC 参数
    - 优化 Java 应用的垃圾回收性能
    - 保持文件编码设置为 UTF-8不变
    - 保持依赖路径配置不变

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git pull
-java -Dfile.encoding=UTF-8 -classpath "./src/main.jar:./target/dependency/*" io.github.gdpl2112.dg_bot.DgMain
+java -Dfile.encoding=UTF-8 -XX:+UseG1GC -classpath "./src/main.jar:./target/dependency/*" io.github.gdpl2112.dg_bot.DgMain
