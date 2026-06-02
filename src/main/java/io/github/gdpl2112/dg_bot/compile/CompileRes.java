482cceb50b6fc751b74a16008aa21e11343cdb23
Kloping <3474006766@qq.com>
2026-06-02 09:47:09 +0800

    chore(version): 更新版本日期常量
    - 将 VERSION_DATE 从 "26/0601" 修改为 "26/0602"

--- src/main/java/io/github/gdpl2112/dg_bot/compile/CompileRes.java
+++ src/main/java/io/github/gdpl2112/dg_bot/compile/CompileRes.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 @Slf4j
 public class CompileRes {
 
-    public static final String VERSION_DATE = "26/0601";
+    public static final String VERSION_DATE = "26/0602";
 
     public static boolean isLinux() {
         String osName = System.getProperty("os.name").toLowerCase();
